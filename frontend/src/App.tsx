5e99626390f9fd3187e9cb2fffdb8af62f26c5f7
lizradway <radwayliz@gmail.com>
2023-04-15 17:11:48 -0400

    Add front end API code for product link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Popup from 'reactjs-popup';
+import {v4 as uuidv4} from 'uuid';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [link, setLink] = useState('');
+  const [type, setType] = useState('');
+
+  let status = ""
+  const apiGatewayEndpoint = "https://s6bc2bia54.execute-api.us-east-1.amazonaws.com/test-stage";
+
+  const handleNameChange = (event: any) => {
+    setName(event.target.value);
+  };
+
+  const handleLinkChange = (event: any) => {
+    setLink(event.target.value);
+  };
+
+  const handleTypeChange = (event: any) => {
+    setType(event.target.value);
+  };
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    const id = uuidv4();
+
+    // Call the API Gateway API with the product ID
+    fetch(`${apiGatewayEndpoint}/products/${id}?name=${name}&link={link}&type=${type}`)
+      .then(response => response.json())
+      .then(data => {
+        // Process the API response data
+        console.log(data);
+        status = "success"
+      })
+      .catch(error => {
+        // Handle API error
+        console.error(error);
+        status = "failure"
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h2>Enter Amazon Product Information:</h2>
+            <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'center', textAlign: 'center' }}>
+              <label>Product Name:</label>
+              <input type="text" value={name} onChange={handleNameChange} placeholder="product name"/>
+
+              <label>Product Link:</label>
+              <input type="text" value={link} onChange={handleLinkChange} placeholder="link"/>
+
+              <label>Product Category:</label>
+              <input type="text" value={type} onChange={handleTypeChange} placeholder="category"/>
+
+                <br></br>
+              <button type="submit" style={{ marginRight: '10px' }}>Submit</button>
+            </form>
       </header>
     </div>
   );
